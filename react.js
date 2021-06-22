b5316e9ff6e22f84977565db4aca24fad812f0ec
AJ <andrew.hosgood@landregistry.gov.uk>
2021-06-22 18:41:47 +0100

    Added eslint-plugin-react-hooks to React config

--- react.js
+++ react.js
@@ -2,12 +2,15 @@ module.exports = {
   extends: [
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
+    "prettier/react",
     "@hmlr/eslint-config"
-
   ],
   // env: {
   //     browser: true
   // },
+  plugins: [
+    "eslint-plugin-react-hooks"
+  ],
   parserOptions: {
     ecmaVersion: 12,
     // sourceType: "script",
